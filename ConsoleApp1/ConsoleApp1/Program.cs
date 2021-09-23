8f4371de5a619307ca14b79aaeb3c0b623ace6bd
Misha <19fishka96@mail.ru>
2021-09-23 22:50:40 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите время в минутах ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (int.TryParse(Int32, out int x))
+            {
+                int h = Convert.ToInt32(x / 60);
+                double m = Convert.ToInt32(x % 60.0);
+                Console.Write("Вам нужно завести будильник на " + h + "часов и" + m + " минут");
+            }
+            else
+            {
+                Console.Write("ОШИБКА");
+            }
+            Console.ReadKey();
+        }
+    }
+}
