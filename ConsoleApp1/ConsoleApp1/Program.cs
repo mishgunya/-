684856648c4579d3aa9dcc4f045b97bcc196d073
Misha <19fishka96@mail.ru>
2021-09-23 23:45:26 +0300

    Code for Katya

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ConsoleApp1
 {
@@ -6,19 +7,27 @@ namespace ConsoleApp1
     {
         private static void Main(string[] args)
         {
-            Console.Write("Введите время в минутах ");
+            Console.Write("Введите время, в которое собираетесь лечь спать (сначала часы, затем минуты) ");
+            int h1 = Convert.ToInt32(Console.ReadLine());
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите время необходимое для сна (в минутах) ");
             int x = Convert.ToInt32(Console.ReadLine());
-            if (int.TryParse(Int32, out int x))
+            int h = Convert.ToInt32(x / 60);
+            double m = Convert.ToInt32(x % 60.0);
+            int h2 = Convert.ToInt32(h1 + h);
+            int m2 = Convert.ToInt32(m1 + m);
+            if (m2 > 60)
             {
-                int h = Convert.ToInt32(x / 60);
-                double m = Convert.ToInt32(x % 60.0);
-                Console.Write("Вам нужно завести будильник на " + h + "часов и" + m + " минут");
+                int h3 = h2 + 1;
+                Console.Write("Вам нужно завести будильник на " + h3 + "часов и" + m2 + " минут");
+                Console.ReadKey();
             }
             else
             {
-                Console.Write("ОШИБКА");
+                Console.Write("Вам нужно завести будильник на " + h2 + "часов и" + m2 + " минут");
+                Console.ReadKey();
             }
-            Console.ReadKey();
+            
         }
     }
 }
